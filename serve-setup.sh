f97927c4e805167543c4cfae16dcf871838e01d6
justnat3 <nd2010@reedusa.com>
2021-02-04 22:36:44 -0600

    some python files

--- serve-setup.sh
+++ serve-setup.sh
@@ -5,3 +5,4 @@ cp /usr/lib/chromium-browser/chromedriver /usr/bin
 pip install selenium, webdriver-manager
 cd schfaliving
 python3 scam-server.py
+https://chromedriver.storage.googleapis.com/88.0.4324.96/chromedriver_linux64.zip
