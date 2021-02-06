5d0dd75a454d3511c04313db41efc1555f1ad3dd
justnat3 <nd2010@reedusa.com>
2021-02-05 19:57:26 -0600

    does not replicate, does not focus

--- scam-server.py
+++ scam-server.py
@@ -3,7 +3,6 @@ import os
 import sys
 import time
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 # funny funny quirky.
@@ -19,37 +18,46 @@ def resource_path(relative_path):
     return os.path.join(base_path, relative_path)
 
 
+iteration = 0
+
+
 def scam_time() -> Scam:
+    global iteration
+    print("INFO: Vote INT:", iteration)
     # load driver
+    print("INFO: OPEN_CHROME_DRIVER")
     opts = webdriver.ChromeOptions()
-    opts.add_argument('--headless')
-    opts.add_argument('--no-sandbox')
-    opts.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome('./driver/chromedriver', chrome_options=opts)
+    opts.add_argument("--headless")
+    opts.add_experimental_option("excludeSwitches", ["enable-logging"])
+    driver = webdriver.Chrome(
+        resource_path("./driver/chromedriver.exe"), chrome_options=opts
+    )
     # go to macky'd's highschool website
+    print("INFO: OPEN_SITE_VYPE")
     driver.get(
         "http://northeast.vypetv.com/hubs/grand-lake/area-news/744-vote-now-neok-area-preseason-most-outstanding-girls-soccer-player-presented-by-northeast-tech-poll-ends-2-10.html?fbclid=IwAR1MCG_p_1ee1mMtOFE3zll5PI0KyMsbQCK34XjND1b6XfZhdNUhvER-0TE"
     )
+    print("INFO: SELECT_ELEMENT_BY_ID_PDI49601961")
     # click on macky'd's name
     driver.find_element_by_id("PDI_answer49601961").click()
     # click on vote button
+    print("INFO: SELECT_ELEMENT_BY_ID_VOTE_BUTTON")
     driver.find_element_by_id("pd-vote-button10735084").click()
     # sleep for 5 seconds
+
     time.sleep(5)
+    print("INFO: CLOSING_CUSTOM_CHROME_DRIVER")
     driver.close()
     driver.quit()
+    iteration += 1
+    print("\n")
     return
 
 
 def scam_position() -> Scam:
-    try:
-        while True:
-            scam_time()
-    except Exception as failure:
+    while True:
         scam_time()
-        
-    # Position is different
-    # ID is the same everytime
-    # PDI_answer49601961
-    # PDI_answer49601961
-scam_position()
+
+
+if __name__ == "__main__":
+    scam_position()
